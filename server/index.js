73b310efccd23b32c54758dd5145f9f214572db3
Dominykas <dominykasdrevinskas@gmail.com>
2022-05-16 01:23:21 +0300

    Updated connection to server

--- server/index.js
+++ server/index.js
@@ -11,8 +11,6 @@ app.use(cors());
 const CONNECTION_URL = 'mongodb+srv://admin:admin123@cluster0.1m42e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => app.listen(PORT, () => console.log(`server running ${PORT}`)))
+mongoose.connect(CONNECTION_URL)
+.then(() => app.listen(PORT, () => console.log(`Server running on: ${PORT}`)))
 .catch((error) => console.log(error.message));
-
-mongoose.set('useFindAndModify', false);

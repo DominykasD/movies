5c2f9a8bb5a515f2f000babea0e8d80be19cd233
Dominykas <dominykasdrevinskas@gmail.com>
2022-05-18 23:54:02 +0300

    Updated styles

--- client/src/components/Movies/Movies.js
+++ client/src/components/Movies/Movies.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Movie from './Movie/Movie.js'
 
+import useStyles from './styles';
+
 const Movies = () => {
+    const classes = useStyles();
+    
     return (
         <>
             <h1>MOVIES</h1>
